a69796418c447aa511cd4df6061ca2f8b90e5d15
maclinelewis <47381672+maclinelewis@users.noreply.github.com>
2020-10-06 12:58:28 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -9,10 +9,6 @@ import os
 
 
 def pytest_sessionfinish(session, exitstatus):
-    data1 = test_calc_add.pytest.json_metadata
-    data2 = test_calc_sub.pytest.json_metadata
-    data3 = test_calc_mul.pytest.json_metadata
-    data4 = test_calc_div.pytest.json_metadata
-    data = data1+data2+data3+data4
+    data = test_calc_sub.pytest.json_metadata
     with open('result.json', 'w') as f:
         json.dump(data, f, indent=4)
